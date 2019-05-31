12451017d702f87b60242a9703fedbaf834d0fe8
rfreeman203 <49911127+rfreeman203@users.noreply.github.com>
2019-05-31 09:39:44 -0400

    Basic Sudo Code
    Beginning Project

---
+++ RF-Happy_Bird.py
@@ -0,0 +1,25 @@
+"""
+C2: Happy Bird
+By: Robert Freeman
+ICS4U   2019/05/30
+
+Project URL: https://drive.google.com/file/d/0B7tap5Q2qs8xYXJ2UUNKelJKZFU/view
+
+Four Cartesian coordinates are read from text file C21.txt (or C22.txt for a second attempt).
+These are the points that will help Happy fly right and avoid all of the obstacles in his way.
+Before he begins his flight, you must determine the coefficients (a, b, c, and d), each rounded
+to two decimal places, of the cubic polynomial y = ax^3 + bx^2 + cx + d so that he can find his
+way unscathed through the gauntlet of pipes ahead of him.
+
+Given a set of coordinates, one per line, each x and y value separated by a single tab character,
+read from text file C21.txt (or C22.txt for a second attempt), calculate and output the value of
+each coefficient. Happy Bird is depending on you!
+
+Example input                  Example Output
+
+1 3                            a = 0.75
+2 4                            b = -5.75
+3 2.5                          c = 13.00
+4 3                            d = -5.00
+"""
+
